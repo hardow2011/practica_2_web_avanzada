42eeaeaaea0c974440457e08fcba5aca95bb761f
hardow2011 <hardow2011@outlook.com>
2020-10-03 16:06:01 -0400

    Constructor, Getters and Setters for the receipt class.

--- src/main/java/com/example/practica_2/entities/Receipt.java
+++ src/main/java/com/example/practica_2/entities/Receipt.java
@@ -29,4 +29,76 @@ public class Receipt {
     private boolean hasBeenReturned;
     @Temporal(TemporalType.DATE)
     private Date returnDate;
+
+    public Receipt(long id, Date rentDate, Date promisedReturnDate, Client client) {
+        this.setId(id);
+        this.setRentDate(rentDate);
+        this.setPromisedReturnDate(promisedReturnDate);
+        this.setClient(client);
+        this.setHasBeenReturned(false);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getRentDate() {
+        return rentDate;
+    }
+
+    public void setRentDate(Date rentDate) {
+        this.rentDate = rentDate;
+    }
+
+    public Date getPromisedReturnDate() {
+        return promisedReturnDate;
+    }
+
+    public void setPromisedReturnDate(Date promisedReturnDate) {
+        this.promisedReturnDate = promisedReturnDate;
+    }
+
+    public List<Equipment> getEquipmentsList() {
+        return equipmentsList;
+    }
+
+    public void setEquipmentsList(List<Equipment> equipmentsList) {
+        this.equipmentsList = equipmentsList;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public boolean isHasBeenReturned() {
+        return hasBeenReturned;
+    }
+
+    public void setHasBeenReturned(boolean hasBeenReturned) {
+        this.hasBeenReturned = hasBeenReturned;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
 }
