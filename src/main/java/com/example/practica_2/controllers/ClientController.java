28ad11dcf15207abf4b2f3bf39fa70ffd4b8b28a
hardow2011 <hardow2011@outlook.com>
2020-10-04 18:15:28 -0400

    Client image received from the form converted to base 64 server side

--- src/main/java/com/example/practica_2/controllers/ClientController.java
+++ src/main/java/com/example/practica_2/controllers/ClientController.java
@@ -1,15 +1,21 @@
 package com.example.practica_2.controllers;
 
+import java.io.IOException;
+
 import com.example.practica_2.entities.Client;
 import com.example.practica_2.services.ClientServices;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/clients")
@@ -20,6 +26,7 @@ public class ClientController {
 
     /**
      * GET method for creating a client
+     *
      * @param model
      * @return
      */
@@ -28,14 +35,26 @@ public class ClientController {
         model.addAttribute("client", new Client());
         model.addAttribute("action", "Crear");
         model.addAttribute("postAddress", "/clients/create");
-        return "createClient";
+        return "createEditViewClient";
     }
 
     @ResponseBody
     @PostMapping("/create")
-    public Client createClient(Client client) {
-        
-        System.out.println("\n\n\n"+client.getBase64Image()+"\n\n\n");
+    public Client createClient(@RequestParam("rawImage") MultipartFile rawImage, Client client) throws IOException {
+
+        // If raw image is not null, otherwise, the image of the client will be "data:image/png;base64,"
+        if(rawImage != null){
+            // To build the final enconded 4 string
+            StringBuilder sb = new StringBuilder();
+            sb.append("data:image/png;base64,");
+
+            // Append to encoded version of the MultipartFile rawImage to the Stringbuilder
+            sb.append(StringUtils.newStringUtf8(Base64.encodeBase64(rawImage.getBytes(), false)));
+
+            // Set the pimage of the client to the string of StringBuilder
+            client.setBase64Image(sb.toString());
+        }
+
         clientServices.save(client);
         return client;
     }
