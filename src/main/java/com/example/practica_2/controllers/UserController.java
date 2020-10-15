186df9fd356e2f22066d9a2fd63c26ed6af795a7
hardow2011 <hardow2011@outlook.com>
2020-10-15 07:59:57 -0400

    user creation

---
+++ src/main/java/com/example/practica_2/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.example.practica_2.controllers;
+
+import com.example.practica_2.entities.Role;
+import com.example.practica_2.entities.User;
+import com.example.practica_2.repository.RoleRepository;
+import com.example.practica_2.services.UserServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("users")
+public class UserController {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserServices userServices;;
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+
+        model.addAttribute("user", new User());
+        model.addAttribute("action", "Crear user");
+        model.addAttribute("postAddress", "/users/create");
+        return "createUpdateViewUser";
+    }
+
+    @PostMapping("/create")
+    public String createUser(User user, @RequestParam(defaultValue = "null") String isAdmin) {
+
+        System.out.println("\n\n\n"+isAdmin);
+        System.out.println(isAdmin.getClass());
+
+        String rolePrefix = "ROLE_";
+        /**
+         * When admin is true from the template, it arrives as false i the controller.
+         * When it is false, it doesn't arrive at all. Hence the manuelly set default "null" value.
+         */
+        if(!isAdmin.equals("null")){
+            Role adminRole = roleRepository.findByName(rolePrefix+"ADMIN");
+            user.addToRolesList(adminRole);
+        }
+
+        userServices.save(user);
+        return "redirect:/users/";
+    }
+}
--- src/main/java/com/example/practica_2/services/UserServices.java
+++ src/main/java/com/example/practica_2/services/UserServices.java
@@ -1,5 +1,8 @@
 package com.example.practica_2.services;
 
+import javax.transaction.Transactional;
+
+import com.example.practica_2.entities.User;
 import com.example.practica_2.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class UserServices {
     @Autowired
     private UserRepository userRepository;
+
+    @Transactional
+    public User save(User user){
+        userRepository.save(user);
+        return user;
+    }
+
 }
